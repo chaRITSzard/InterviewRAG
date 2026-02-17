c21309bbba4a62945ec18c7e29fb98baca033bb5
ritvik <ritviksny@gmail.com>
2026-02-17 23:43:25 +0530

    Prepared the loader function

--- loader.py
+++ loader.py
@@ -1,2 +1,15 @@
-def loader(a):
-    return a
+from pypdf import PdfReader
+
+def load_resume(pdf_path: str) -> str:
+
+    reader = PdfReader(pdf_path)
+    pages = reader.pages
+    text = ""
+    for page in pages:
+        extract = page.extract_text()
+        if extract is not None:
+            text += extract
+    return text
+
+test = load_resume('CoverLetter.pdf')
+print(test)
